1261f6fcd324c3e7f624806bad666ed4288d6f47
sirliuyang <sirliuyang@qq.com>
2017-05-20 17:30:09 +0800

    add file2matrix

--- KNN.py
+++ KNN.py
@@ -1,31 +1,53 @@
 from numpy import *
 import  operator
 
-def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0.0,0.0],[0.0,0.1]])
-    labels =['A','A','B','B']
-    return  group,labels
+class KNN():
+    def createDataSet(self):
+        group = array([[1.0,1.1],[1.0,1.0],[0.0,0.0],[0.0,0.1]])
+        labels =['A','A','B','B']
+        return  group,labels
 
-def classify0(inX,dataSet,labels,k):
-    dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX,(dataSetSize,1)) - dataSet
+    def classify0(self,inX,dataSet,labels,k):
+        dataSetSize = dataSet.shape[0]
+        diffMat = tile(inX,(dataSetSize,1)) - dataSet
+        sgDiffMat = diffMat**2
+        sgDistances = sgDiffMat.sum(axis=1)
+        distances = sgDistances**0.5
+        sorteDistIndicies = distances.argsort()
+        classCount = {}
+        for i in range(k):
+            voteIlable =  labels[sorteDistIndicies[i]]
+            print voteIlable
+            classCount [voteIlable] = classCount.get(voteIlable,0)+1
+            print classCount
+        sortedClassCount = sorted(classCount.iteritems(),key = operator.itemgetter(1),reverse=True)
+        return sortedClassCount[0][0]
 
-    sgDiffMat = diffMat**2
+    def file2matrix(selft,filename):
+        fr = open(filename)
+        numberOfLines = len(fr.readlines())  # get the number of lines in the file
+        returnMat = zeros((numberOfLines, 3))  # prepare matrix to return
+        classLabelVector = []  # prepare labels return
+        fr = open(filename)
+        index = 0
+        for line in fr.readlines():
+            line = line.strip()
+            listFromLine = line.split('\t')
+            returnMat[index, :] = listFromLine[0:3]
+            classLabelVector.append(int(listFromLine[-1]))
+            index += 1
+        return returnMat, classLabelVector
 
-    sgDistances = sgDiffMat.sum(axis=1)
+    def autoNorm(dataSet):
+        minVals = dataSet.min(0)
+        maxVals = dataSet.max(0)
+        ranges = maxVals - minVals
+        normDataSet = zeros(shape(dataSet))
+        m = dataSet.shape[0]
+        normDataSet = dataSet - tile(minVals, (m, 1))
+        normDataSet = normDataSet / tile(ranges, (m, 1))  # element wise divide
+        return normDataSet, ranges, minVals
+k = KNN()
 
-    distances = sgDistances**0.5
-    #print diffMat,sgDiffMat,sgDistances,distances
-    sorteDistIndicies = distances.argsort()
-    print distances,sorteDistIndicies
-    classCount = {}
-    for i in range(k):
-        voteIlable =  labels[sorteDistIndicies[i]]
-        print voteIlable
-        classCount [voteIlable] = classCount.get(voteIlable,0)+1
-        print classCount
-    sortedClassCount = sorted(classCount.iteritems(),key = operator.itemgetter(1),reverse=True)
-    return sortedClassCount[0][0]
-
-group,labels = createDataSet()
-print(classify0([2,1],group,labels,4))
+group,labels = k.createDataSet()
+print(k.classify0([2,1],group,labels,4))
